5f24855108cc9c3d92244dcef6df7133750f9cab
richard hannah <richard.hannah1974@gmail.com>
2025-07-14 21:29:43 +0100

    change indentation

--- orchestrate.sh
+++ orchestrate.sh
@@ -58,10 +58,10 @@ for row in $(echo "$MANIFEST" | jq -r '.repos[] | @base64'); do
     repo=$(cut -d/ -f2 <<< "$github_path")
 
 create_response=$(curl -sS -X POST \
-  -H "Authorization: Bearer $ASO_PAT" \
-  -H "Accept: application/vnd.github+json" \
-  https://api.github.com/orgs/$org/repos \
-  -d @- <<'EOF'
+-H "Authorization: Bearer $ASO_PAT" \
+-H "Accept: application/vnd.github+json" \
+https://api.github.com/orgs/$org/repos \
+-d @- <<'EOF'
 {
   "name": "'"$repo"'",
   "private": true,
